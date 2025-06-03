79e4aa169c044cf33a1c3f7cdcd9c77c6912d86c
Marc Gavanier <marc.gavanier@protonmail.com>
2025-06-03 23:59:14 +0200

    feat: add field info errors prop

--- src/libraries/react-form/field/info.field.tsx
+++ src/libraries/react-form/field/info.field.tsx
@@ -2,15 +2,24 @@ import { ReactNode } from 'react';
 import { hasError } from '@/libraries/react-form/field/has-error';
 import { useFieldContext } from '../form-context';
 
-export const Info = ({ className = 'text-error mt-1 text-xs' }: { className?: string }): ReactNode => {
-  const { state } = useFieldContext<string>();
+const hasExactlyOne = <T,>(items: T[]): items is [T] => items.length === 1;
 
-  return hasError(state) ? (
-    state.meta.errors.length === 1 ? (
-      <p className={className}>{state.meta.errors[0].message}</p>
+export const Info = ({
+  errors: errorsProp = [],
+  className = 'text-error mt-1 text-xs'
+}: {
+  errors?: Error[];
+  className?: string;
+}): ReactNode => {
+  const { state } = useFieldContext<string>();
+  const errors: Error[] = [...state.meta.errors, ...errorsProp];
+
+  return hasError(state) || errorsProp.length > 0 ? (
+    hasExactlyOne(errors) ? (
+      <p className={className}>{errors[0].message}</p>
     ) : (
       <ul className={className}>
-        {state.meta.errors.map(({ message }) => (
+        {errors.map(({ message }) => (
           <li key={message}>{message}</li>
         ))}
       </ul>
